10d163ddeaec4d1cf7dcfc1f5acd30bcf9bd8a17
Soon Chee Loong <scheeloong@gmail.com>
2015-06-07 19:17:01 -0700

    Updated testing script for outbreak as previous was wrong file used

--- Outbreak/self_test.py
+++ Outbreak/self_test.py
@@ -8,14 +8,6 @@ import outbreak
 def test_sample():
     """ A simple test of functions get_distances and visit_next.
     """
-    # Create the distance dictionary-- assumes the data file exists.
-    distances = outbreak.get_distances("cities.txt")
-    # Start off with the outbreak city on the unvisited list.
-    visited = []
-    unvisited = [("Toronto", 0)]
-    # Transfer cities to the visited list until there are no cities left.
-    while unvisited:
-        outbreak.visit_next(visited, unvisited, distances)
-    print visited
+    print outbreak.visit_next([('Toronto',0)], [('New York',3),('Mexico City', 7), ('San Francisco',6)],outbreak.get_distances("cities.txt"))
 if __name__ == "__main__":
     test_sample()
